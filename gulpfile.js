9be9378de289f72b01aeaba3442549b1b5932efb
Ilya <ilya.soloveyv@gmail.com>
2018-09-14 18:47:43 +0300

    fix gulp and s9

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ gulp.task('js_min', () => {
             'public/src/js/bootstrap.js',
             'public/src/js/jquery.fullpage.min.js',
             'public/src/js/owl.carousel.min.js',
+            'public/src/js/jquery.mask.min.js',
             'public/src/js/app/app.js',
             'public/src/js/app/index.js',
             'public/src/js/index/s2.js',
