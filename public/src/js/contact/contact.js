fead6696fad00fdf680a56f007ef7e6682a1f416
Ilya <ilya.soloveyv@gmail.com>
2018-09-14 14:50:24 +0300

    fix contact js

--- public/src/js/contact/contact.js
+++ public/src/js/contact/contact.js
@@ -1,3 +1,1 @@
-$(document).ready(function(){
-    $('#contact main .contacts__form .left-block input[name=phone]').mask('+7 (000) 000-00-00');
-});
+$('#contact main .contacts__form .left-block input[name=phone]').mask('+7 (000) 000-00-00');
