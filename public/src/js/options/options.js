a9a9397bfec674239142db8ebed8c1dbd5689238
alexdanulyk <alexdanulyk.inc@gmail.com>
2018-10-05 00:20:23 +0300

    up 05.10

---
+++ public/src/js/options/options.js
@@ -0,0 +1,16 @@
+$(function () {
+	
+	$('.menu__icon_burger').click(function(e) {
+		e.preventDefault();
+		$('.upmenu').addClass('upmenu__active');		
+		$('section').addClass('blur');
+	});
+
+	$('.upmenu__close').click(function(){
+		$('.upmenu').removeClass('upmenu__active');		
+		$('section').removeClass('blur');
+	});
+
+	
+	
+});
