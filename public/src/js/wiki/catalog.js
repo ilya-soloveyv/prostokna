8506e1897b8909449ce314c4a2e6e7456cfe31be
Your Name <you@example.com>
2018-11-29 16:00:08 +0300

    wiki and article

--- public/src/js/wiki/catalog.js
+++ public/src/js/wiki/catalog.js
@@ -1,7 +1,7 @@
 if ($('#wiki_catalog').length) {
 
     function wiki_catalog_num () {
-        $('#wiki_catalog ul li').each(function(i, v){
+        $('#wiki_catalog ul.catalog_list li').each(function(i, v){
             let num = i+1;
                 num = (String(num).length == 1) ? '0'+num : num;
             $(this).find('.num').text(num);
@@ -14,10 +14,30 @@ if ($('#wiki_catalog').length) {
         $.ajax({
             url: "/wiki/get"
         }).done(function(html) {
-            $('#wiki_catalog ul').append(html)
+            $('#wiki_catalog ul.catalog_list').append(html)
             wiki_catalog_num()
         });
         return false;
+    })
+
+    $('#wiki_catalog .hamburger').click(function(){
+        if ($(this).hasClass('is-active')) {
+            $(this).removeClass('is-active')
+            $('#wiki_catalog .sidebar').removeClass('open')
+            $('#wiki_catalog ul').removeClass('disabled')
+            // $('html, body').css({overflow: 'auto'})
+        } else {
+            $(this).addClass('is-active')
+            $('#wiki_catalog .sidebar').addClass('open')
+            $('#wiki_catalog ul').addClass('disabled')
+            // $('html, body').css({overflow: 'hidden'})
+        } 
+    })
+
+    $('#wiki_catalog .sidebar ul li a').mouseenter(function(){
+        $('#wiki_catalog .sidebar ul li a').not(this).css('opacity', 0.5);
+    }).mouseleave(function(){
+        $('#wiki_catalog .sidebar ul li a').css('opacity', 1);
     })
 
 
