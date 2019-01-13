8b509ea5112440b35bd66afca89dc2d9370d81b1
AlexDanulyk <alexdanulyk.inc@gmail.com>
2019-01-13 17:59:59 +0300

    Add to page

--- public/src/js/regulation_window/regulation_window.js
+++ public/src/js/regulation_window/regulation_window.js
@@ -1,1 +1,14 @@
-console.log('regulation_window')
+$(document).ready(function () {
+  $('.btn-desc').click(function () { 
+    $('.pop-up').fadeIn();    
+  });
+  $('.pop-up_close').click(function () { 
+    $('.pop-up').fadeOut();    
+  });
+
+  $(".regulation_window-checkitem").click(function () {
+    
+    $(".regulation_window-checkitem").removeClass('active');
+    $(this).addClass('active');
+  })
+});
