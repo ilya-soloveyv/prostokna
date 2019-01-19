e468fb2ee2bf0828a7d24822e019d84e08634951
Your Name <you@example.com>
2019-01-19 16:40:32 +0300

    bug fix left menu

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ const async = require('async')
 const cookieParser = require('cookie-parser')
 const fs = require('fs')
 app.use(cookieParser())
-
 app.locals.env = process.env;
-
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.use('/vue', express.static(__dirname + '/node_modules/vue/dist'))
 
-var left_menu = [
+var data = {}
+data.left_menu = [
     {
         title: 'Главная',
         uri: '/',
@@ -59,24 +58,23 @@ var left_menu = [
         ico: [ '8.svg', '8a.svg' ]
     },
 ]
-var left_menu_active = false;
 
-app.use(function (req, res, next) {
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-        var randomNumber = Math.random().toString()
-        randomNumber = randomNumber.substring(2,randomNumber.length)
-        res.cookie('cookieName', randomNumber, { maxAge: 900000, httpOnly: true })
-    } else {
-        console.log('cookie exists', cookie)
-    }
-    next()
-});
+// cookie
+// app.use(function (req, res, next) {
+//     var cookie = req.cookies.cookieName;
+//     if (cookie === undefined) {
+//         var randomNumber = Math.random().toString()
+//         randomNumber = randomNumber.substring(2,randomNumber.length)
+//         res.cookie('cookieName', randomNumber, { maxAge: 900000, httpOnly: true })
+//     } else {
+//         console.log('cookie exists', cookie)
+//     }
+//     next()
+// });
 
 app.get('/', (req, res) => {
-    left_menu_active = 0
-    var data = {}
-        data.s2_menu = [
+    data.title = 'Просто окна'
+    data.s2_menu = [
         {
             title: 'Окна',
             img: '1.svg',
@@ -670,7 +668,7 @@ app.get('/', (req, res) => {
             ]
         },
     ]
-    res.render('index.pug', { title: 'Просто окна', data: data, left_menu: left_menu })
+    res.render('index.pug', data)
 })
 
 app.get('/calc_data', (req, res) => {
@@ -681,15 +679,17 @@ app.get('/calc_data', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact.pug', { title: 'Контакты' })
+    data.title = 'Контакты'
+    res.render('contact.pug', data)
 })
 
 app.get('/gager', (req, res) => {
-    res.render('gager.pug', { title: 'Замерщик' })
+    data.title = 'Замерщик'
+    res.render('gager.pug', data)
 })
 
 app.get('/product', (req, res) => {
-    left_menu_active = 1
+    data.title = 'Окна'
     let connection = mysql.createConnection(config.get('db'))
     let query = "SELECT * FROM product t1 LEFT JOIN brand t2 ON t2.iBrandID = t1.iBrandID";
     connection.query(query, (err, rows, fields) => {
@@ -698,14 +698,13 @@ app.get('/product', (req, res) => {
             res.sendStatus(500)
             return
         }
-        let products = rows;
-        res.render('product/products.pug', { products: products, left_menu_active: left_menu_active })
+        data.products = rows;
+        res.render('product/products.pug', data)
     })
     connection.end()
 })
 
 app.get('/product/:sProductURI', (req, res) => {
-    left_menu_active = 1
     var connection = mysql.createConnection(config.get('db'));
     var query_1 = "SELECT * FROM product t1 LEFT JOIN brand t2 ON t2.iBrandID = t1.iBrandID WHERE t1.sProductURI = ? LIMIT 1";
     var query_2 = "SELECT * FROM product_images WHERE iProductID = ? ORDER BY iOrder ASC";
@@ -714,7 +713,6 @@ app.get('/product/:sProductURI', (req, res) => {
     var query_5 = "SELECT * FROM product_images WHERE iPhotoInDescOnPage = 1 && iProductID = ? LIMIT 1";
     var query_6 = "SELECT t1.sProductURI, t1.sProductTitle, t2.sBrandTitle FROM product t1 LEFT JOIN brand t2 ON t2.iBrandID = t1.iBrandID";
     var query_7 = "SELECT t1.iProductID, t2.sColorCode, t2.sColorTitle, t2.sColorTitleCode, t2.sColorTitle, t2.sColorTextureFileName FROM product_color t1 LEFT JOIN colors t2 ON t2.iColorID = t1.iColorID WHERE t1.iProductID = ?";
-    var data = {};
     async.parallel([
        function(parallel_done) {
            connection.query(query_1, [ req.params.sProductURI ], function(err, product) {
@@ -779,66 +777,80 @@ app.get('/product/:sProductURI', (req, res) => {
 })
 
 app.get('/pay', (req, res) => {
-    res.render('pay.pug', { title: 'Оплата' })
+    data.title = 'Оплата'
+    res.render('pay.pug', data)
 })
 
 app.get('/palette', (req, res) => {
-    res.render('palette.pug', { title: 'Palette' })
+    data.title = 'Палитра'
+    res.render('palette.pug', data)
 })
 
 app.get('/options', (req, res) => {
-    res.render('options.pug', { title: 'Options' })
+    data.title = 'Опции'
+    res.render('options.pug', data)
 })
 
-var catalog = []
+
+
+data.catalog = []
 var catalog_item = {
     title: "Особые стекла для пластиковых окон ПВХ",
     desc: "Стеклопакет – важнейший элемент ПВХ окна. От его качества зависят ведущие свойства окна из пластика в целом – шумоизоляция, параметры сохранения тепла в жилище и даже безопасность проживающих в помещении. Чтобы пластиковые окна ПВХ"
 }
 for (let index = 0; index < 10; index++) {
-    catalog.push(catalog_item);
+    data.catalog.push(catalog_item);
 }
 
 app.get('/wiki', (req, res) => {
-    res.render('wiki/catalog.pug', { title: 'Wiki', catalog: catalog })
+    data.title = 'Вики'
+    res.render('wiki/catalog.pug', data)
 })
 
 app.get('/wiki/get', (req, res) => {
-    res.render('wiki/catalog_list.pug', { catalog: catalog })
+    res.render('wiki/catalog_list.pug', data)
 })
 
 app.get('/wiki/article', (req, res) => {
-    res.render('wiki/article.pug', { title: 'Wiki article' })
+    data.title = 'Wiki article'
+    res.render('wiki/article.pug', data)
 })
 
 app.get('/instruction', (req, res) => {
-    res.render('instruction/instruction.pug', { title: 'Instruction' })
+    data.title = 'Instruction'
+    res.render('instruction/instruction.pug', data)
 })
 
 app.get('/instruction/video', (req, res) => {
-    res.render('instruction/video.pug', { title: 'Instruction Video' })
+    data.title = 'Instruction Video'
+    res.render('instruction/video.pug', data)
 })
 
 app.get('/company', (req, res) => {
-    res.render('company/company.pug', { title: 'О компании' })
+    data.title = 'О компании'
+    res.render('company/company.pug', data)
 })
 
 app.get('/corporate', (req, res) => {
-    res.render('corporate/corporate.pug', { title: 'Corporate' })
+    data.title = 'Corporate'
+    res.render('corporate/corporate.pug', data)
 })
 
 
 
 app.get('/regulation_window', (req, res) => {
-    res.render('regulation_window/regulation_window.pug', { title: 'Регулировка окон' })
+    data.title = 'Регулировка окон'
+    res.render('regulation_window/regulation_window.pug', data)
 })
 
 app.get('/optional_service', (req, res) => {
-    res.render('optional_service/optional_service.pug', { title: 'Дополнительные услуги' })
+    data.title = 'Дополнительные услуги'
+    res.render('optional_service/optional_service.pug', data)
 })
 
 app.get('/intuitive', (req, res) => {
-    res.render('intuitive/intuitive.pug', { title: 'Интуйтивный подбор окон' })
+    data.title = 'Интуйтивный подбор окон'
+    res.render('intuitive/intuitive.pug', data)
 })
 
 
