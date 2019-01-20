7d31339dffe15affb1eac939fdd9284c94c81317
AlexDanulyk <alexdanulyk.inc@gmail.com>
2019-01-21 00:41:52 +0300

    Fixed intuinive

--- public/src/js/intuitive/intuitive.js
+++ public/src/js/intuitive/intuitive.js
@@ -1,3 +1,11 @@
 if ($("#intuitive").length) {
     console.log('intuitive')
 }
+
+$(document).ready(function () {
+    $('.choise-btn').click(function (e) { 
+        e.preventDefault();
+        $('.intuitive-bref').fadeOut(100);
+        $('.intuitive-wrap').fadeIn(700);
+    });
+});
