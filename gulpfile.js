4992f4fac394adcdcbe45c922da515997a50a19a
ilya <ilya.soloveyv@gmail.com>
2019-04-24 17:46:02 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task('js_min', (done) => {
             'public/src/js/bootstrap.js',
             'public/src/js/jquery.fullpage.min.js',
             'public/src/js/owl.carousel.min.js',
+            '/node_modules/jquery-mousewheel/jquery.mousewheel.js',
             'node_modules/slick-carousel/slick/slick.min.js',
             'public/src/js/rangeslider.min.js',
             'public/src/js/jquery.scrollbar.min.js',
