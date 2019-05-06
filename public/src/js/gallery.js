dd972bb586a66a83ce0f16dfcc2c22ed1479dedb
ilya <ilya.soloveyv@gmail.com>
2019-05-06 08:04:03 +0300

    fix

--- public/src/js/gallery.js
+++ public/src/js/gallery.js
@@ -14,6 +14,7 @@ if ($("#gallery").length) {
     })
 
     $(function () {
+        $('body').css({overflow: 'hidden'})
         $('[data-toggle="popover"]').popover({
           container: 'body'
         })
