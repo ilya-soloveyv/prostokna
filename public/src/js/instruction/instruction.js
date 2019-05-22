48e88b206c8a7f7f7d480b24d496598dc8108c07
AlexDanulyk <alexdanulyk.inc@gmail.com>
2019-05-22 17:05:25 +0300

    Fix instruction

--- public/src/js/instruction/instruction.js
+++ public/src/js/instruction/instruction.js
@@ -1,4 +1,13 @@
-if ($('.instruction').length) {	
+if ($('.instruction').length) {
+
+	// Tabs script
+
+	$('.instruction__tabs .instruction__tabs_content').on('click', function () {
+		$(this)
+			.addClass('active').siblings().removeClass('active').closest('.instruction').find('.instruction__wrapp').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
+	// Accordion script
 
 	$('.instruction__accor .acc__title').on('click', f_acc);
 
@@ -6,31 +15,104 @@ if ($('.instruction').length) {
 		$('.instruction__wrapp.active .instruction__accor .acc__content').not($(this).next()).slideUp();
 
 		$(this).next().slideToggle();
-	}
+	}		
+
+	$("a.go_to").click(function (e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+	// Window nums script
 
 	$('.instruction__wrapp .num .num__item').on('click', function(){
-		$(this).addClass('active').siblings().removeClass('active').closest('.instruction__wrapp').find('.instruction__accor .acc__title').eq($(this).index()).click();
+    var item  = $(this).index();
+
+		$(this).toggleClass('active').siblings().removeClass('active').toggleClass('none').closest('.instruction__wrapp').find('.instruction__accor .acc__title').eq($(this).index()).click();
+
+		$(this).closest('.instruction__img').find('.text .text__item').eq($(this).index()).toggleClass('active');
+
+		$('.up').toggleClass('active');
+
+    if (item == 0) {
+			$.each($('.part'), function(i, el) {
+        setTimeout(function() {
+          $(el).toggleClass("active");
+        }, 100 + (i * 100));
+
+      });      
+			// window
+			$('.op1').toggleClass('active');
+			$('.dn1').toggleClass('active');
+			$('.dn12').toggleClass('active');
+			$('.dc1').toggleClass('color');
+			// balcony
+			$('.b-line1').toggleClass('color');
+			$('.b-line2').toggleClass('none');
+
+		  
+		}
+    if (item == 1) {
+			$.each($('.part'), function(i, el) {
+        setTimeout(function() {
+          $(el).toggleClass("active");
+        }, 100 + (i * 100));
+
+      });      
+
+			$('.op1').toggleClass('active');
+			$('.dn12').toggleClass('active');
+			$('.dn2').toggleClass('active');
+			$('.dc2').toggleClass('color');
+      // balcony
+			$('.b-line2').toggleClass('color');
+			$('.b-line1').toggleClass('none');
+			$('.balcony-center').toggleClass('op3');
+			$('.b-window').toggleClass('op3');
+
+
+		  
+		}
+    if (item == 2) {			     
+
+			$('.line3').toggleClass('color');
+			$('.part').toggleClass('op3');
+			$('#center').toggleClass('op3');
+			$('.line').toggleClass('op3');
+			$('.under').toggleClass('op3');
+			// balcony
+			$('.b-line2').toggleClass('none');
+			$('.b-line1').toggleClass('none');
+			$('#balcony-right').toggleClass('show');
+		  
+		}
+    if (item == 3) {			     
+   
+			$('.line4').toggleClass('db4');
+			$('.part').toggleClass('op3');
+			$('#center').toggleClass('op3');
+			$('.line').toggleClass('op3');
+			$('.under').toggleClass('op3');
+			$('#max-down').toggleClass('op3');
+		  
+		}
+    if (item == 4) {			     
+   
+			$('.line5').toggleClass('db4');
+			$('.part').toggleClass('op3');
+			$('#center').toggleClass('op3');
+			$('.line').toggleClass('op3');
+			$('.under').toggleClass('op3');
+			$('#max-down').toggleClass('op3');
+		  
+		}
+
 
 	});
 	
-
-	var it = $('.instruction__wrapp.active .num__item:first-child');
-	if ($(it).hasClass('active') == true) {
-		$('.instruction__wrapp.active .instruction__accor .acc__title:first-child').click();
-	}
-	
-	$('input').on('change', function(){
-		var trg = $('.instruction__wrapp');
-		var check = $(this).data('check');
-
-		if (check == 1) {
-			$(trg).first().addClass('active');
-			$(trg).last().removeClass('active');
-		} else if (check == 2) {
-			$(trg).first().removeClass('active');
-			$(trg).last().addClass('active');
-		}		
-	})
+	// Description more script
 
 	$('.instruction__wrapp .instruction__btn').on('click',function(){
 		$(this).toggleClass('active');
