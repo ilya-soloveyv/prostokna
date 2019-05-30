726aa950abdece560c415a971b922841a95cd82e
ilya <ilya.soloveyv@gmail.com>
2019-05-31 02:15:41 +0300

    fix

--- public/src/js/product/product.js
+++ public/src/js/product/product.js
@@ -32,6 +32,7 @@ $(document).ready(function(){
             slidesToScroll: 1,
             vertical: true,
             verticalSwiping: true,
+            infinite: false,
             prevArrow: '<button type="button" class="slick-prev"><i class="material-icons">keyboard_arrow_up</i></button>',
             nextArrow: '<button type="button" class="slick-next"><i class="material-icons">keyboard_arrow_down</i></button>'
         })
@@ -43,7 +44,8 @@ $(document).ready(function(){
         
         $("#product .data .basic .gallery .view2 .front").stop().animate({ width: '50%' }, 500)
 
-        $('#product .data .basic .gallery .list2 .slick-change').on("click", function(){
+        $('#product .data .basic .gallery .list2 .slick-change').on("click", function(e){
+            let index = $(this).attr('data-slick-index')
             let iProductID = $(this).attr('data-iProductID')
             let iProductImageID = $(this).attr('data-iProductImageID')
             let sProductImageFrontName = $(this).attr('data-sProductImageFrontName')
@@ -51,10 +53,13 @@ $(document).ready(function(){
 
             $('#product .data .basic .gallery .view2 .front .bg img')
                 .attr('src', '/images/product/gallery/' + sProductImageFrontName)
-                .attr('data-iProductImageID', iProductImageID);
+                .attr('data-iProductImageID', iProductImageID)
+                .attr('data-index', index)
 
             if (typeof sProductImageBackName !== 'undefined') {
-                $("#product .data .basic .gallery .view2 .back .bg img").attr('src', '/images/product/gallery/' + sProductImageBackName).show()
+                $("#product .data .basic .gallery .view2 .back .bg img")
+                    .attr('src', '/images/product/gallery/' + sProductImageBackName)
+                    .attr('data-index', index).show()
                 $("#product .data .basic .gallery .view2 .front").stop().animate({
                     width: $("#product .data .basic .gallery .view2").width()/2
                 }, 150)
@@ -94,6 +99,61 @@ $(document).ready(function(){
             $("#product .selectOption .item").removeClass('active')
             $(this).addClass('active')
         })
+
+        // Модалка с галлереей
+        var owl_product_modal_images = $("#product_modal_images .owl-carousel")
+
+        $("#product .data .basic .gallery .view2").click(function(e){
+            if (e.target.localName == 'img') {
+                var index = Number($(e.target).attr('data-index'))
+
+                $('#product .data .basic .gallery .list2 .slick .slick-list .slick-track .slick-slide').each(function (e, i) {
+                    var img = $(this).find('img').attr('src')
+                    owl_product_modal_images.trigger('add.owl.carousel', [
+                        `<div class="item">
+                            <img src="` + img + `">
+                        </div>`
+                    ])
+                })
+
+                console.log(index)
+                owl_product_modal_images.trigger('to.owl.carousel', [index, 250])
+
+                $("#product_modal_images").modal()
+            }
+        })
+        
+        owl_product_modal_images.on('changed.owl.carousel', function(event) {
+
+        })
+        owl_product_modal_images.on('initialized.owl.carousel', function(event) {
+            
+        })
+        owl_product_modal_images.owlCarousel({
+            items: 1,
+            nav: true,
+            dots: true
+        })
+
+        $('#product_modal_images').on('show.bs.modal', function (event) {
+
+        }).on('shown.bs.modal', function (event) {
+            $('html, body').css({overflow: "hidden", position: "fixed"})
+            $('body').addClass('modal-backdrop-wiki').css({background: '#353535'})
+            $("#product .toggle_submenu .bg .icons .hamburger").addClass('is-active')
+            $("#product_modal_images").find('.hamburger').addClass('is-active')
+            owl_product_modal_images.addClass('open')
+        }).on('hide.bs.modal', function (event) {
+            $('html, body').css({overflow: "auto", position: "static"})
+            $('body').removeClass('modal-backdrop-wiki').css({background: '#FFF'})
+            $("#product .toggle_submenu .bg .icons .hamburger").removeClass('is-active')
+            $("#product_modal_images").find('.hamburger').removeClass('is-active')
+            owl_product_modal_images.removeClass('open')
+            $('#product_modal_images .owl-item').each(function(i, el){
+                owl_product_modal_images.trigger('remove.owl.carousel', i)
+            })
+        })
+        //  *** *** *** *** *** *** *** *** *** *** *** *** //
 
     }
 });
