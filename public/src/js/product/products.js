c98216973a6d60b4bba7da8ccd353635990f709b
ilya <ilya.soloveyv@gmail.com>
2019-07-03 22:45:31 +0300

    fix

--- public/src/js/product/products.js
+++ public/src/js/product/products.js
@@ -8,30 +8,31 @@ $(document).on("click", ".productsMenu__brands__brand a", function () {
 })
 
 
-// $(document).on("click", ".brand_list_link", function () {
-//     $(this).parents('.brand_list_item').toggleClass('open').siblings().removeClass('open')
-//     return false
-// }).on("click", "#productMenu .list .item .title ul li", function () {
-//     $(this).addClass('active').siblings().removeClass('active')
-//     return false
-// }).on("click", "#productMenu .filter .category ul li a", function () {
-//     $(this).parent().addClass('active').siblings().removeClass('active')
-//     return false
-// })
+$(document).on("click", ".brand_list_link", function () {
+    $(this).parents('.list').find('.brand_list_item').removeClass('open')
+    $(this).parents('.brand_list_item').addClass('open')
+    return false
+}).on("click", "#productMenu .list .item .title ul li", function () {
+    $(this).addClass('active').siblings().removeClass('active')
+    return false
+}).on("click", "#productMenu .filter .category ul li a", function () {
+    $(this).parent().addClass('active').siblings().removeClass('active')
+    return false
+})
 
 
 
-// var productsMenu = new Vue({
-//     el: '#productMenu',
-//     data: {
-//         mess: 'Hello Vue'
-//     },
-//     created: function () {
+var productsMenu = new Vue({
+    el: '#productMenu',
+    data: {
+        mess: 'Hello Vue'
+    },
+    created: function () {
         
-//     },
-//     methods: {
-//         test: function () {
+    },
+    methods: {
+        test: function () {
 
-//         },
-//     }
-// })
+        },
+    }
+})
