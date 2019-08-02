10a1a3f8c0a5f89c9043b7630e10e9a7e7f0c358
AlexDanulyk <alexdanulyk.inc@gmail.com>
2019-08-02 23:31:23 +0300

    Fix 4 layout

--- public/src/js/index/s8.js
+++ public/src/js/index/s8.js
@@ -1,15 +1,17 @@
 if ($('#fullpage_welcome').length) {
     // $('#fullpage_welcome .s8 input[name=tel]').inputmask("+7 (999) 999-99-99")
     select_s8(0)
-    $(document).on("click", "#fullpage_welcome .s8 .l ul li", function () {
+	$(document).on("click", "#fullpage_welcome .s8 .flex-block ul li", function () {
         select_s8($(this).index())
-    })
+    });
+
+
 }
 
 function select_s8 (index) {
-    var subject = $("#fullpage_welcome .s8 .l ul li").eq(index).attr('data-subject')
+	var subject = $("#fullpage_welcome .s8 .flex-block ul li").eq(index).attr('data-subject')
     $("#fullpage_welcome .s8 form").find('input[name=subject]').val(subject)
-    $('#fullpage_welcome .s8 .l ul li').removeClass('active')
-    $('#fullpage_welcome .s8 .l ul li').eq(index).addClass('active')
+	$('#fullpage_welcome .s8 .flex-block ul li').removeClass('active')
+	$('#fullpage_welcome .s8 .flex-block ul li').eq(index).addClass('active')
 }
     
