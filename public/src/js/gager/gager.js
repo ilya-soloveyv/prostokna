f152bc96ef4a225f9d3ae67ea10d18da50f9e597
AlexDanulyk <alexdanulyk.inc@gmail.com>
2019-08-19 16:07:43 +0300

    Add page gager

--- public/src/js/gager/gager.js
+++ public/src/js/gager/gager.js
@@ -1,1 +1,66 @@
-// Inputmask({"mask": "+7 (999) 999-99-99"}).mask("#gager input[name=phone]")
+if ($('#gager').length) {
+	Inputmask({ "mask": "+7 (999) 999-99-99" }).mask("#gager input[name=phone]")
+	// плавная прокрутака
+	var $page = $('html, body');
+	$('a[href*="#"]').click(function () {
+		$page.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 600);
+		return false;
+	});
+	// Если поля не пустые оставлем описание наверху
+	$('.gager-form_main input').change(function () {
+		if ($(this).val().length) {
+			$(this).siblings('label').css('transform', 'translateY(-32px)');
+		}
+	});
+
+	// Скрипт лоадера после отправки формы
+	var circles = $('.ico');
+	$('.form-submit').on('click', function(e) {
+		// Отменяю отправку для примера анимации
+		e.preventDefault();
+		$('.gager-form_main').hide();
+		$('.overlay').css({
+			'background': 'rgba(0, 0, 0, 0.3)'
+		});
+
+
+		// Сама анимация svg
+		$('.ellipse').css({
+			'stroke-dasharray': '1400, 0'
+		});
+
+		$.each(circles, function (i, el) {
+			setTimeout(function () {
+				$(el).addClass('active')
+			}, (i * 375));
+		});
+
+		setTimeout(() => {			
+			circles.removeClass('active');
+			$('.popup-thanks').show();
+			$('.ellipse').css({
+				'stroke-dasharray': '0, 1400'
+			});
+			$('.overlay').css({
+				'background': 'rgba(0, 0, 0, 0.9)'
+			});
+			if ($('.popup-thanks').is(':visible')) {
+				$('.popup-thanks').on('click', function () {
+					$('.popup-thanks').hide();
+					$('.gager-form_main').show();
+					$('.overlay').css({
+						'background': 'rgba(0, 0, 0, 0.7)'
+					});
+				});
+				
+			}
+		}, 3000);
+
+		
+	});
+
+	// Возвращение к норме
+	
+}
