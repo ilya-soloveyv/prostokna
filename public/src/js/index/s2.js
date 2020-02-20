8b5167cbd50b98e64df46d51518e2ddab9cd9485
Sergey Timshin <timshinsergey1@gmail.com>
2020-02-20 15:27:03 +0400

    s2 final

--- public/src/js/index/s2.js
+++ public/src/js/index/s2.js
@@ -24,7 +24,7 @@ $(document).on("mouseenter", "#fullpage_welcome .section.s2 ul.level1 li.level1"
 
 
 $('.s2-secondary-card a.more').click(function() {
-    $('.s2-secondary-list li').removeClass('show');
+    $('.s2-secondary-card').removeClass('show');
 
     $( this ).closest('li').addClass('show');
 
@@ -32,23 +32,40 @@ $('.s2-secondary-card a.more').click(function() {
 })
 
 if (screen.width < 800) {
-    $('.s2-control--menu').click(function() {
+    $('.s2-secondary-list').slick();
+    $('.s2-secondary-list').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+        $('.s2-secondary-card').removeClass('show');
+
+        $('.slick-slide').removeClass('is-active show');
+
+        $('.slick-active').addClass('is-active show');
+    });
+
+
+    $('.s2-menu').click(function() {
         $('.s2-secondary-list').removeClass('open');
-        $('.s2-secondary-list li').removeClass('show');
+        $('.s2-secondary-card').removeClass('show is-active');
+
+        $('.s2-menu').removeClass('active');
     })
 
     $('.s2-primary-card').click(function() {
         $('.s2-secondary-list').addClass('open');
+
+        $('.s2-menu').addClass('active');
         
-        $('.s2-secondary-list li').removeClass('is-active show');
+        $('.s2-secondary-card').removeClass('is-active show');
         $('.s2-secondary-list > li').eq($(this).index()).addClass('is-active show');
+
+
+        $('.s2-secondary-list').slick('slickGoTo', $(this).index());
     });
 } else {
     $('.s2-primary-card').click(function() {
         $('.s2-primary-card').removeClass('is-active').addClass('is-inactive');
         $( this ).removeClass('is-inactive').addClass('is-active');
     
-        $('.s2-secondary-list li').removeClass('is-active show');
+        $('.s2-secondary-card').removeClass('is-active show');
         $('.s2-secondary-list > li').eq($(this).index()).addClass('is-active show');
     });
 }
