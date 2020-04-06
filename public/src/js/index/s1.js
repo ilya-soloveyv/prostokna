1e5788c38576bfe74d2a1a7feaa12c03754f8543
Sergey Timshin <timshinsergey1@gmail.com>
2020-04-06 20:33:53 +0400

    index

--- public/src/js/index/s1.js
+++ public/src/js/index/s1.js
@@ -1,41 +1,42 @@
-if (screen.width < 800) {
-    $('.s1 .s1-items').slick({
-        dots: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        arrows: false,
-    });
-
-    $('.s1 .s1-items').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-        let imgUrl = `url(${$('.s1 .slick-active > a').data().img})`;
-
+if (document.querySelector('.layout-index')) {
+    if (screen.width < 800) {
+        $('.s1 .s1-items').slick({
+            dots: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+        });
+    
+        $('.s1 .s1-items').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            let imgUrl = `url(${$('.s1 .slick-active > a').data().img})`;
+    
+            $(".s1 .bg-image").stop().animate({opacity: 0},300,function(){
+                $(this).css({'background-image': imgUrl})
+                           .animate({opacity: 0.5},{duration:300});
+             });
+        });
+    } else {
+        $('.s1 .s1-items a').hover(function() {
+            if ($( this ).hasClass('is-hovered')) {
+                return;
+            }
+    
+            $('.s1 .s1-items a').removeClass('is-hovered');
+            $( this ).addClass('is-hovered');
+    
+            s1ShowImage($(this));
+        });
+    }
+    
+    s1ShowImage($('.s1 .is-hovered'))
+    
+    function s1ShowImage(sel) {
+        let imgUrl = `url(${sel.data().img})`;
+    
         $(".s1 .bg-image").stop().animate({opacity: 0},300,function(){
             $(this).css({'background-image': imgUrl})
                        .animate({opacity: 0.5},{duration:300});
-         });
-    });
-} else {
-    $('.s1 .s1-items a').hover(function() {
-        if ($( this ).hasClass('is-hovered')) {
-            return;
-        }
-
-        $('.s1 .s1-items a').removeClass('is-hovered');
-        $( this ).addClass('is-hovered');
-
-        s1ShowImage($(this));
-    });
+        });
+    }   
 }
-
-s1ShowImage($('.s1 .is-hovered'))
-
-function s1ShowImage(sel) {
-    let imgUrl = `url(${sel.data().img})`;
-
-    $(".s1 .bg-image").stop().animate({opacity: 0},300,function(){
-        $(this).css({'background-image': imgUrl})
-                   .animate({opacity: 0.5},{duration:300});
-    });
-} 
-
