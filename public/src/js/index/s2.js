0d162407893f3a68ae60477cd1b5f3f00646e45e
Sergey Timshin <timshinsergey1@gmail.com>
2020-04-06 21:16:09 +0400

    fix product page

--- public/src/js/index/s2.js
+++ public/src/js/index/s2.js
@@ -1,104 +1,106 @@
-if (document.querySelector('.layout-index') && document.body.offsetWidth > 910) {
-    const s2Cards = document.querySelectorAll('.s2-main-list > li');
-    const s2CloseCardBtns = document.querySelectorAll('.s2-front-card .card-close');
-    const s2BackCardBtns = document.querySelectorAll('.s2-front-card .card-back');
-    const s2CardLinks = document.querySelectorAll('.s2-back-card .more');
+if (document.querySelector('.layout-index')) {
+    if (document.body.offsetWidth > 910) {
+        const s2Cards = document.querySelectorAll('.s2-main-list > li');
+        const s2CloseCardBtns = document.querySelectorAll('.s2-front-card .card-close');
+        const s2BackCardBtns = document.querySelectorAll('.s2-front-card .card-back');
+        const s2CardLinks = document.querySelectorAll('.s2-back-card .more');
 
-    s2Cards.forEach(card => {
-        card.addEventListener('click', event => {
-            let target = event.target.closest('.s2-card');
+        s2Cards.forEach(card => {
+            card.addEventListener('click', event => {
+                let target = event.target.closest('.s2-card');
 
-            if (target.classList.contains('active')) return;
+                if (target.classList.contains('active')) return;
 
-            s2Cards.forEach(el => {
-                el.classList.add('inactive');
-                el.classList.remove('active');
-            });
+                s2Cards.forEach(el => {
+                    el.classList.add('inactive');
+                    el.classList.remove('active');
+                });
 
-            target.classList.remove('inactive');
-            target.classList.add('active');
-            
+                target.classList.remove('inactive');
+                target.classList.add('active');
+                
+                hiddenCards();
+
+                if (target.querySelector('.s2-back-card').offsetHeight != target.querySelector('.s2-back-card').scrollHeight) {
+                    target.classList.add('isScroll')
+                }
+
+                console.log(target.querySelector('.s2-back-card').offsetHeight != target.querySelector('.s2-back-card').scrollHeight);
+            })
+        });
+
+        s2CloseCardBtns.forEach(btn => {
+            btn.addEventListener('click', event => {
+                s2Cards.forEach(el => {
+                    el.classList.remove('active');
+                    el.classList.remove('inactive');
+                });
+
+                hiddenCards();
+
+                event.stopPropagation();
+            })
+        });
+
+        s2CardLinks.forEach(link => {
+            link.addEventListener('click', event => {
+                let target = event.target;
+
+                showLowerLevelsLists(true);
+
+                target.nextElementSibling.classList.add('isShown');
+
+                event.preventDefault();
+                event.stopPropagation();
+            })
+        })
+
+        s2BackCardBtns.forEach(btn => {
+            btn.addEventListener('click', event => {
+                let shownList = document.querySelectorAll('.isShown');
+
+                if (!shownList.length) return;
+
+                shownList[shownList.length - 1].classList.remove('isShown');
+
+                if (shownList.length === 1) showLowerLevelsLists(false);
+            })
+        });
+    } else {
+        const s2cards = document.querySelectorAll('.s2-cards .s2-front-card');
+        const s2CloseBtn = document.querySelector('.s2-close');
+        const s2CardLinks = document.querySelectorAll('.s2-first-level .more');
+
+        s2cards.forEach((card, index) => {
+            card.addEventListener('click', event => {
+                let target = event.target.closest('.s2');
+                target.classList.add('isActive');
+
+                console.log(document.querySelector(`.s2-list-${index}`));
+                
+                fullpage_api.moveTo('s2', index);
+            })
+        })
+
+        s2CardLinks.forEach(link => {
+            link.addEventListener('click', event => {
+                let target = event.target;
+
+                target.closest('.s2-first-level').classList.add('isActive');
+
+                target.nextElementSibling.classList.add('isShown');
+
+                event.preventDefault();
+                event.stopPropagation();
+            })
+        })
+
+        s2CloseBtn.addEventListener('click', () => {
             hiddenCards();
 
-            if (target.querySelector('.s2-back-card').offsetHeight != target.querySelector('.s2-back-card').scrollHeight) {
-                target.classList.add('isScroll')
-            }
-
-            console.log(target.querySelector('.s2-back-card').offsetHeight != target.querySelector('.s2-back-card').scrollHeight);
-        })
-    });
-
-    s2CloseCardBtns.forEach(btn => {
-        btn.addEventListener('click', event => {
-            s2Cards.forEach(el => {
-                el.classList.remove('active');
-                el.classList.remove('inactive');
-            });
-
-            hiddenCards();
-
-            event.stopPropagation();
-        })
-    });
-
-    s2CardLinks.forEach(link => {
-        link.addEventListener('click', event => {
-            let target = event.target;
-
-            showLowerLevelsLists(true);
-
-            target.nextElementSibling.classList.add('isShown');
-
-            event.preventDefault();
-            event.stopPropagation();
-        })
-    })
-
-    s2BackCardBtns.forEach(btn => {
-        btn.addEventListener('click', event => {
-            let shownList = document.querySelectorAll('.isShown');
-
-            if (!shownList.length) return;
-
-            shownList[shownList.length - 1].classList.remove('isShown');
-
-            if (shownList.length === 1) showLowerLevelsLists(false);
-        })
-    });
-} else {
-    const s2cards = document.querySelectorAll('.s2-cards .s2-front-card');
-    const s2CloseBtn = document.querySelector('.s2-close');
-    const s2CardLinks = document.querySelectorAll('.s2-first-level .more');
-
-    s2cards.forEach((card, index) => {
-        card.addEventListener('click', event => {
-            let target = event.target.closest('.s2');
-            target.classList.add('isActive');
-
-            console.log(document.querySelector(`.s2-list-${index}`));
-            
-            fullpage_api.moveTo('s2', index);
-        })
-    })
-
-    s2CardLinks.forEach(link => {
-        link.addEventListener('click', event => {
-            let target = event.target;
-
-            target.closest('.s2-first-level').classList.add('isActive');
-
-            target.nextElementSibling.classList.add('isShown');
-
-            event.preventDefault();
-            event.stopPropagation();
-        })
-    })
-
-    s2CloseBtn.addEventListener('click', () => {
-        hiddenCards();
-
-        document.querySelector('.s2').classList.remove('isActive');
-    });
+            document.querySelector('.s2').classList.remove('isActive');
+        });
+    }
 }
 
 function showLowerLevelsLists(bool) {
