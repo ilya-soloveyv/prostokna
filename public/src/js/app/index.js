c821c55a905756ecea5a885beabeed3ee627b39c
Sergey Timshin <timshinsergey1@gmail.com>
2020-04-09 16:43:46 +0400

    index modals

--- public/src/js/app/index.js
+++ public/src/js/app/index.js
@@ -13,7 +13,7 @@ const toggleImg = (target) => {
     }
 
     $(".s1 .bg-image").stop().animate({opacity: 0},300, function() {
-        $(this).css({'background-image': imgUrl}).animate({opacity: 0.5},{duration:300})
+        $(this).css({'background-image': imgUrl}).animate({opacity: clientWidth() < 800 ? 0.5 : 1},{duration:300})
     })
 }
 
@@ -36,6 +36,68 @@ const s1CreateEventsForLinks = () => {
         })
     })
 }
+
+const upload = () => {
+    var dropZone = $('.drop-wrap');
+
+	$('[type="file"]').focus(function() {
+		$('label').addClass('focus');
+	})
+	.focusout(function() {
+		$('label').removeClass('focus');
+	});
+
+
+	dropZone.on('drag dragstart dragend dragover dragenter dragleave drop', function(){
+		return false;
+	});
+
+	dropZone.on('dragover dragenter', function() {
+		dropZone.addClass('dragover');
+	});
+
+	dropZone.on('dragleave', function(e) {
+		let dx = e.pageX - dropZone.offset().left;
+		let dy = e.pageY - dropZone.offset().top;
+		if ((dx < 0) || (dx > dropZone.width()) || (dy < 0) || (dy > dropZone.height())) {
+			dropZone.removeClass('dragover');
+		}
+	});
+
+	dropZone.on('drop', function(e) {
+		dropZone.removeClass('dragover');
+		let files = e.originalEvent.dataTransfer.files;
+		sendFiles(files);
+	});
+
+	$('[type="file"]').change(function() {
+		let files = this.files;
+		sendFiles(files);
+	});
+
+
+	function sendFiles(files) {
+		let maxFileSize = 5242880;
+		let Data = new FormData();
+		$(files).each(function(index, file) {
+			if ((file.size <= maxFileSize) && ((file.type == 'image/png') || (file.type == 'image/jpeg'))) {
+				Data.append('images[]', file);
+			}
+		});
+
+		$.ajax({
+			url: dropZone.attr('action'),
+			type: dropZone.attr('method'),
+			data: Data,
+			contentType: false,
+			processData: false,
+			success: function(data) {
+				alert ('Файлы были успешно загружены!');
+			}
+		});
+	}
+}
+  
 
 function fullpage_welcome_color(i) {
     let index = i.index;
@@ -76,11 +138,73 @@ $("#fullpage_nav li").click(function(){
     $.fn.fullpage.moveTo(i+1);
 });
 
-function indexShowImage(imgPath, target) {
-    let imgUrl = `url(${imgPath})`;
+if (window.FileReader) { 
+    //var drop; 
+    addEventHandler(window, 'load', function() {
+        // const status = document.querySelector('.drop-status')
+        const drop = document.querySelector('.drop')
+        // const list = document.querySelector('.drop-list')
+  
+        function cancel(e) {
+            if (e.preventDefault) e.preventDefault()
 
-    $(target).stop().animate({opacity: 0},200,function(){
-        $(this).css({'background-image': imgUrl})
-                   .animate({opacity: 1},{duration:200});
-    });
+            return false
+        }
+  
+        // Tells the browser that we *can* drop on this target
+        addEventHandler(drop, 'dragover', cancel)
+        addEventHandler(drop, 'dragenter', cancel)
+        addEventHandler(drop, 'drop', function (e) {
+            e = e || window.event // get window.event if e argument missing (in IE)   
+    
+            if (e.preventDefault) e.preventDefault() // stops the browser from redirecting off to the image.
+    
+            var dt = e.dataTransfer
+            var files = dt.files
+
+            for (var i=0; i<files.length; i++) {
+                var file = files[i]
+                var reader = new FileReader()
+    
+                //attach event handlers here...
+                reader.readAsDataURL(file)
+                addEventHandler(reader, 'loadend', function(e, file) {
+                    let bin = this.result
+
+                    // console.log(bin)
+                    // console.log(file)
+    
+                    showFiles(file)
+    
+                }.bindToEventHandler(file))
+            }
+            return false
+        })
+  
+        Function.prototype.bindToEventHandler = function bindToEventHandler() {
+            var handler = this
+            var boundParameters = Array.prototype.slice.call(arguments)
+            //create closure
+            return function(e) {
+                e = e || window.event // get window.event if e argument missing (in IE)   
+                boundParameters.unshift(e)
+                handler.apply(this, boundParameters)
+            }
+        }
+    })
+} else { 
+    document.getElementsByClassName('status')[0].innerHTML = 'Ваш браузер не поддерживает HTML5 FileReader.';
+}
+
+function addEventHandler(obj, evt, handler) {
+    if(obj.addEventListener) {
+        // W3C method
+        obj.addEventListener(evt, handler, false);
+    } else if(obj.attachEvent) {
+        // IE method.
+        obj.attachEvent('on'+evt, handler);
+    } else {
+        // Old school method.
+        obj['on'+evt] = handler;
+    }
 }
