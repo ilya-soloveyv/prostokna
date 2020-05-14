b0228d33aeeb8aa62eb2632ea4bc7da454f3a5e5
paramonies <paramonies@yandex.ru>
2020-05-14 22:56:04 +0300

    s8

--- public/src/js/index/s8.js
+++ public/src/js/index/s8.js
@@ -2,76 +2,100 @@ $(".section.s8 .fp_bg .flex-block .bottom-block .type-block ul li").on("click", 
     $(this).addClass('active').siblings().removeClass('active');
 });
 
-
+Vue.directive('phone', {
+  bind(el) {  
+    el.oninput = function(e) {
+      if (!e.isTrusted) {
+        return;
+      }
+      if(this.value.length >= 2) {
+        var s = this.value;
+        this.value = s.substr(3);
+      }
+      let x = this.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+      this.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '');
+      this.value='+7 ' + this.value;
+      el.dispatchEvent(new Event('input'));
+    }
+  }
+});
 
 var app = new Vue({
-    el: '#submitForm',
-    data: {
-      files: [],
-      isNone:  '',
-      hideSubmit: false
+  el: '#submitForm',
+  data: {
+    files: [],
+    isNone:  '',
+    hideSubmit: false,
+    phone: '',
+    showMessage: false
+  },
+  mounted() {
+    Vue.nextTick(function() {
+      // $("#phoneInput").inputmask({"mask": "+7(999) 999-99-99"});
+    })
+  },
+  methods: {
+    addFiles(){
+      this.$refs.files.click();
     },
-    mounted() {
-      Vue.nextTick(function() {
-        $("#phoneInput").inputmask({"mask": "+7(999) 999-99-99"});
-      })
-    },
-    methods: {
-      addFiles(){
-        this.$refs.files.click();
-      },
 
-      submitFiles(){
-        Vue.set(this, 'hideSubmit', true);
-        var v = this;
-        setTimeout(
-          function() { 
-            Vue.set(v, 'hideSubmit', false);
-          }, 
-          3000
-        );
-        let formData = new FormData();
-        for( var i = 0; i < this.files.length; i++ ){
-          let file = this.files[i];
-          formData.append('files[' + i + ']', file);
-        }
-        // axios.post( '/select-files',
-        //   formData,
-        //   {
-        //     headers: {
-        //         'Content-Type': 'multipart/form-data'
-        //     }
-        //   }
-        // ).then(function(){
-        //   console.log('SUCCESS!!');
-        // })
-        // .catch(function(){
-        //   console.log('FAILURE!!');
-        // });
-      },
-
-      handleFilesUpload(){
-        let uploadedFiles = this.$refs.files.files;
-        for( var i = 0; i < uploadedFiles.length; i++ ){
-          this.files.push( uploadedFiles[i] );
-        }
-        console.log('handleFileUpload', this.files.length);
-        if(this.files.length >= 3) {
-            this.isNone='none';
-          }
-      },
-      removeFile( key ){
-        this.files.splice( key, 1 );
-        if(this.files.length < 3) {
-            this.isNone='';
-          }
-      },
-      makeShortName( fileName) {
-        var ext = fileName.substr(fileName.lastIndexOf('.'));
-        if(fileName.length > 20) {
-          fileName = fileName.substr(0, 10) + '...' + fileName.substr(name.length-5);
-        }
-        return fileName;
+    submitFiles(){
+      if(this.phone.length<18) {
+        this.showMessage = true;
+        return
       }
+      Vue.set(this, 'hideSubmit', true);
+      var v = this;
+      setTimeout(
+        function() { 
+          Vue.set(v, 'hideSubmit', false);
+        }, 
+        3000
+      );
+      this.showMessage = false;
+      let formData = new FormData();
+      for( var i = 0; i < this.files.length; i++ ){
+        let file = this.files[i];
+        formData.append('files[' + i + ']', file);
+      }
+      // axios.post( '/select-files',
+      //   formData,
+      //   {
+      //     headers: {
+      //         'Content-Type': 'multipart/form-data'
+      //     }
+      //   }
+      // ).then(function(){
+      //   console.log('SUCCESS!!');
+      // })
+      // .catch(function(){
+      //   console.log('FAILURE!!');
+      // });
+    },
+
+    handleFilesUpload(){
+      let uploadedFiles = this.$refs.files.files;
+      for( var i = 0; i < uploadedFiles.length; i++ ){
+        this.files.push( uploadedFiles[i] );
+      }
+      console.log('handleFileUpload', this.files.length);
+      if(this.files.length >= 3) {
+          this.isNone='none';
+        }
+    },
+    removeFile( key ){
+      this.files.splice( key, 1 );
+      if(this.files.length < 3) {
+          this.isNone='';
+        }
+    },
+    makeShortName( fileName) {
+      var ext = fileName.substr(fileName.lastIndexOf('.'));
+      if(fileName.length > 20) {
+        fileName = fileName.substr(0, 10) + '...' + fileName.substr(name.length-5);
+      }
+      return fileName;
     }
-  })    
+  }
+})  
+  
