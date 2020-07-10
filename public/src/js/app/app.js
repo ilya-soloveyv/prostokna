41f84eae19c3f0a93e077b587974c817d8c6d195
ilya <ilya.soloveyv@gmail.com>
2020-07-10 13:04:21 +0300

    fix

--- public/src/js/app/app.js
+++ public/src/js/app/app.js
@@ -269,6 +269,44 @@ $('#modal_gallery_owl').on('show.bs.modal', function (event) {
 
 
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // при необходимости добавьте другие значения по умолчанию
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+const cookie_modal_scammers = getCookie('modal_scammers')
+if (!cookie_modal_scammers) {
+	$("#modal_scammers").modal().on('hide.bs.modal', function (event) {
+		setCookie('modal_scammers', true, {'max-age': 3600000});
+	})
+}
+
 
 
 
